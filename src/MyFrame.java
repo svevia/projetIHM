add34c683edddd67f06c14a1835df5b7b0a4d8d1
Mamadou <uodamam@hotmail.com>
2015-05-27 10:21:45 +0200

    Update img

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,10 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -7,6 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 
 public class MyFrame {
@@ -16,14 +22,34 @@ public class MyFrame {
 	    	 JFrame f = new JFrame("Connexion") ;
 	    	 JPanel panel = new JPanel(); 
 	    	 panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS)) ; 
+	    	 //TextField Recherche
 	    	 JTextField recherche = new JTextField(20) ; 
+	    	 
+	    	 //Panel Search
 	    	 panel.add(recherche) ;
+	    	 
+	    	 
+	    	 //Button Disconnect 
 	    	 JButton deconnecte = new JButton(new ImageIcon("images/power.png")) ; 
-	         panel.add(deconnecte) ; 
+	    	 deconnecte.setBackground(Color.WHITE); 
+	    	 deconnecte.setPreferredSize(new Dimension(28,28)) ;
+	    	 deconnecte.setBorderPainted(false);
+	    	 
+	        
+	    	 //add JButton to JPanel
+	    	 panel.add(deconnecte) ; 
+	         
 	    	 
 	    	 
 	    	 
-	    	 f.getContentPane().add(panel) ; 
+	         
+	         //Principal JPanel
+	         JPanel general = new JPanel() ; 
+	         //Add Panel1 to General 
+	         general.add(panel) ; 
+	    	 
+	         //Parameter JFrame
+	         f.getContentPane().add(general) ; 
 	    	 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
 	    	 f.pack() ; 
 	    	 f.setVisible(true) ; 
