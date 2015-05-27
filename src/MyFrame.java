ece7a8d56d9e8b8b4c7c63b446b0d5a4b2b06131
Mamadou <uodamam@hotmail.com>
2015-05-27 09:48:55 +0200

    Add Image

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,4 +1,42 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 
 public class MyFrame {
 
+	
+	     public MyFrame(){
+	    	 JFrame f = new JFrame("Connexion") ;
+	    	 JPanel panel = new JPanel(); 
+	    	 panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS)) ; 
+	    	 JTextField recherche = new JTextField(20) ; 
+	    	 panel.add(recherche) ;
+	    	 JButton deconnecte = new JButton(new ImageIcon("images/power.png")) ; 
+	         panel.add(deconnecte) ; 
+	    	 
+	    	 
+	    	 
+	    	 f.getContentPane().add(panel) ; 
+	    	 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
+	    	 f.pack() ; 
+	    	 f.setVisible(true) ; 
+	     }
+	     
+	     public static void main(String[] args){
+	    	 javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				
+				@Override
+				public void run() {
+					new MyFrame() ; 
+					
+				}
+			});
+	     }
 }
